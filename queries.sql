aafaeb51ddb71c1e8226d8e4b82187b315f1b3cd
raythurman2386 <raymondthurman5@gmail.com>
2019-12-29 00:16:23 -0500

    possible second query, not sure yet

--- queries.sql
+++ queries.sql
@@ -9,6 +9,10 @@ ON Product.CategoryId = Category.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+SELECT OrderId, CompanyName FROM Order
+JOIN Shipper ON Order.ShipVia = Shipper.CompanyName
+WHERE OrderDate < "2012_08_09";
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
